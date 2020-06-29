8855e04946cb89770d601de8215b096272b0b61b
Nick20017 <56848959+Nick20017@users.noreply.github.com>
2020-06-29 15:32:08 +0300

    Add files via upload

---
+++ js/zoomImg.js
@@ -0,0 +1,14 @@
+﻿function zoomImg(mouseOver, id, range) {
+    var element = document.getElementById(id);
+    var width = element.clientWidth;
+    var height = element.clientHeight;
+    range = 150;
+    if (mouseOver && id != null) {
+        element.style.width = (width + range) + 'px';
+        element.style.height = (height + range) + 'px';
+    }
+    else if (!mouseOver && id != null) {
+        element.style.width = (width - range) + 'px';
+        element.style.height = (height - range) + 'px';
+    }
+}
